caebee497dce8983b3f1d45ec008442af296b98a
Augustine Victor <augustinevickky@gmail.com>
2023-09-16 10:12:10 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var width = window.innerWidth
+
+const socials = document.getElementsByClassName('socials')
+
+
+function changeSocialButtonPosition() {
+    if(width <= 689){
+       
+    }else{
+     
+
+    }
+}
+
+changeSocialButtonPosition()
